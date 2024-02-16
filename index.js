8f4d29af4dd11ea11b07c673e28f15135f06260a
Olesya <krayanskaya_ov@mail.ru>
2024-02-16 22:06:14 +0300

    рефакторинг

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const addTaskFormBtn = document.getElementById('add-task-form');
 const addTaskBtn = document.getElementById('add-task');
 const addTaskForm = document.querySelector('.add');
 const taskSection = document.querySelector('.tasks__list');
-const taskSortBtn = document.getElementById('sort');
+const taskFilterBtn = document.getElementById('sort');
 
 let tasksList = new Map();
 let id = 3;
@@ -12,7 +12,7 @@ let id = 3;
 const taskTitleInput = document.getElementById('task-title');
 const taskDescriptionInput = document.getElementById('task-description');
 
-function addTask() {
+function setNewTaskData() {
     let task = {
         id: (id += 1),
         title: null,
@@ -34,63 +34,82 @@ function clearInputs() {
     taskDescriptionInput.value = null;
 }
 
-function createTaskItem(task) {
+function createTask(task) {
+
     let newTask = createNewNode('li', 'tasks__item task');
     newTask.setAttribute('id', task.id);
     taskSection.append(newTask);
 
-    let newLiHeader = createNewNode('header', 'task__header');
-    newTask.append(newLiHeader);
+    let taskDescription = createNewNode('p', 'task__description');
+    taskDescription.innerHTML = task.description;
 
-    let newTaskTitle = createNewNode('h3', 'task__title');
-    newTaskTitle.innerHTML = task.title;
-    newLiHeader.append(newTaskTitle);
+    newTask.append(createHeader(task, taskDescription, newTask));
+    newTask.append(taskDescription);
+    newTask.append(createFooter(task, taskDescription));
+}
 
-    let newTaskAction = createNewNode('div', 'task__action');
-    newLiHeader.append(newTaskAction);
+function createFooter(task, taskDescription) {
 
-    let newTaskDescription = createNewNode('p', 'task__description');
-    newTaskDescription.innerHTML = task.description;
-    newTask.append(newTaskDescription);
+    let taskFooter = createNewNode('footer', 'task__footer');
 
-    let newTaskFooter = createNewNode('footer', 'task__footer');
-    newTask.append(newTaskFooter);
-
-    let newTaskBtnClose = createNewNode('button', 'task__btn-close');
-    newTaskBtnClose.innerHTML = 'скрыть';
-    newTaskBtnClose.addEventListener('click', () => {
-        newTaskDescription.style.display = 'none';
+    let btnClose = createNewNode('button', 'task__btn-close');
+    btnClose.innerHTML = 'скрыть';
+    btnClose.addEventListener('click', () => {
+        taskDescription.style.display = 'none';
     });
-    newTaskFooter.append(newTaskBtnClose);
+    taskFooter.append(btnClose);
 
-    let newTaskDate = createNewNode('span', 'task__date');
-    newTaskDate.innerHTML = task.date.toLocaleDateString();
-    newTaskFooter.append(newTaskDate);
+    let taskDate = createNewNode('span', 'task__date');
+    taskDate.innerHTML = `Создана ${task.date.toLocaleDateString()}`;
+    taskFooter.append(taskDate);
 
-    let newTaskBtn = createNewNode('button', 'task__btn-desc');
-    newTaskBtn.innerHTML = 'описание';
-    newTaskBtn.addEventListener('click', () => {
-        newTaskDescription.style.display = 'block';
+    return taskFooter;
+}
+
+function createHeader(task, taskDescription, newTask) {
+
+    let taskHeader = createNewNode('header', 'task__header');
+
+    let taskTitle = createNewNode('h3', 'task__title');
+    taskTitle.innerHTML = task.title;
+    taskHeader.append(taskTitle);
+
+    let newTaskAction = createAction(task, taskDescription, newTask);
+    taskHeader.append(newTaskAction);
+
+    return taskHeader;
+}
+
+function createAction(task, taskDescription, newTask) {
+
+    let taskAction = createNewNode('div', 'task__action');
+
+    let descriptionBtn = createNewNode('button', 'task__btn-desc');
+    descriptionBtn.innerHTML = 'описание';
+    descriptionBtn.addEventListener('click', () => {
+        taskDescription.style.display = 'block';
     });
-    newTaskAction.append(newTaskBtn);
+    taskAction.append(descriptionBtn);
 
-    let newTaskCheck = createNewNode('input', 'task__check');
-    newTaskCheck.type = 'checkbox';
-    newTaskCheck.addEventListener('change', () => {
+    let isDoneCheckbox = createNewNode('input', 'task__check');
+    isDoneCheckbox.type = 'checkbox';
+    isDoneCheckbox.addEventListener('change', () => {
         task.isDone = !task.isDone;
     });
     task.isDone === true
-        ? newTaskCheck.setAttribute('checked', true)
-        : newTaskCheck.removeAttribute('checked');
-    newTaskAction.append(newTaskCheck);
+        ? isDoneCheckbox.setAttribute('checked', true)
+        : isDoneCheckbox.removeAttribute('checked');
+    taskAction.append(isDoneCheckbox);
 
-    let newTaskBtnDel = createNewNode('button', 'task__btn-del');
-    newTaskBtnDel.innerHTML = 'удалить';
-    newTaskBtnDel.addEventListener('click', () => {
+    let deleteTaskBtn = createNewNode('button', 'task__btn-del');
+    deleteTaskBtn.innerHTML = 'удалить';
+    deleteTaskBtn.addEventListener('click', () => {
         deleteTaskFromList(task.id);
         newTask.remove();
     });
-    newTaskAction.append(newTaskBtnDel);
+    taskAction.append(deleteTaskBtn);
+
+    return taskAction;
 }
 
 function createNewNode(nodeTag, className) {
@@ -123,26 +142,26 @@ addTaskFormBtn.addEventListener('click', () => {
 
 addTaskBtn.addEventListener('click', () => {
     addTaskForm.style.display = 'none';
-    let task = addTask();
-    createTaskItem(task);
+    let task = setNewTaskData();
+    createTask(task);
     clearInputs();
 });
 
-taskSortBtn.addEventListener('change', () => {
-    switch (taskSortBtn.value) {
+taskFilterBtn.addEventListener('change', () => {
+    switch (taskFilterBtn.value) {
         case 'все':
             removeAllChildNodes(taskSection);
-            tasksList.forEach((task) => createTaskItem(task));
+            tasksList.forEach((task) => createTask(task));
             break;
         case 'выполненные':
             let isDoneTasks = filterTasks(true);
             removeAllChildNodes(taskSection);
-            isDoneTasks.forEach((task) => createTaskItem(task));
+            isDoneTasks.forEach((task) => createTask(task));
             break;
         case 'активные':
             let isActiveTasks = filterTasks(false);
             removeAllChildNodes(taskSection);
-            isActiveTasks.forEach((task) => createTaskItem(task));
+            isActiveTasks.forEach((task) => createTask(task));
             break;
     }
 });
